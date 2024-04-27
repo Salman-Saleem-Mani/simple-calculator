ea99838e2ae3a54466ebb8c0d98cd98c182bdc16
--replace-all <[salmansaleemriz@gmail.com]>
2024-04-27 06:17:01 +0500

    PUSH MY FIRST SIMPLE CALCULATOR TO GITHUB

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+//shabang
+
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+  {
+    message: "select one of the operators to perdorm operations",
+    type: "list",
+    name: "Operators",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+console.log(answer)
+
+//condition statement
+if(answer.Operators==="Addition"){
+    console.log(answer.FirstNumber + answer.SecondNumber)
+}else if(answer.Operators==="Subtraction"){
+    console.log(answer.FirstNumber - answer.SecondNumber)
+}else if(answer.Operators==="Multiplication"){
+    console.log(answer.FirstNumber * answer.SecondNumber)
+}else if(answer.Operators==="Division"){
+    console.log(answer.FirstNumber / answer.SecondNumber)
+}else{
+    console.log("select a valid operator")
+}
